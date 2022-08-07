95abdb11a7eb33b0676f13dc110c5c4d22f9e026
«Denis» <denvoropaev7@gmail.com>
2022-08-07 11:53:54 +0300

    Задача, которая принимает три  числа и выдает максимальное

---
+++ seminar01_dz4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;    
+}
+Console.WriteLine("Максимальное число = ");
+Console.WriteLine(max);
+    
