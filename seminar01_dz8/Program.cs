6fdf64e0107f6403878eed6ef15e13b251319086
«Denis» <denvoropaev7@gmail.com>
2022-08-07 12:21:14 +0300

    Задача, которая принимает число, а на выходе показывает все четныен числа от 1 до n

---
+++ seminar01_dz8/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+    for(int i = 1;i<=number;i++){
+        if (i%2 == 0){
+        Console.WriteLine(i);
+        }
+    }
