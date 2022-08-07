b2f68dc7b0ef405b6a357ba515be3292a1803751
«Denis» <denvoropaev7@gmail.com>
2022-08-07 10:54:25 +0300

    Задача, которая выдает день недели по номеру

---
+++ seminar01_z3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите номер: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number) {
+    case 1:
+    Console.WriteLine("Понедельник");
+break;
+    case 2:
+    Console.WriteLine("Вторник");
+break;
+     case 3:
+    Console.WriteLine("Среда");
+break;
+    case 4:
+    Console.WriteLine("Четверг");
+break;
+    case 5:
+    Console.WriteLine("Пятница");
+break;
+    case 6:
+    Console.WriteLine("Суббота");
+break;
+    case 7:
+    Console.WriteLine("Воскресенье");
+break;                   
+}
