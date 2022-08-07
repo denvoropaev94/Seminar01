32355f57f633d52dc39c8d423070df2d9e5f34ff
«Denis» <denvoropaev7@gmail.com>
2022-08-07 11:12:15 +0300

    Задача, которая принимает трёхзначное  число и выводит 3-ю цифру

---
+++ seminar01_z5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number>100 & number < 1000){
+Console.WriteLine(number%10);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число!!!");
+}
+
