08bbf9a9580b039ed8c48513468fe330b531edbb
«Denis» <denvoropaev7@gmail.com>
2022-08-07 11:31:14 +0300

    Задача, которая принимает два числа и выдает максимальное

---
+++ seminar01_dz2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.Write("a>b, max = ");
+    Console.Write(a);
+}
+else
+{
+    Console.Write("b>a, max = ");
+    Console.Write(b);
+}
