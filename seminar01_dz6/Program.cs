8dc1dbdedc33dcc5d2f772c9bff8d720479d0c77
«Denis» <denvoropaev7@gmail.com>
2022-08-07 12:00:29 +0300

    Задача, которая принимает число и выдает является ли оно четным

---
+++ seminar01_dz6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2 == 0){
+    Console.WriteLine("Число четное");
+}
+else{
+    Console.WriteLine("Число нечетное");
+}
