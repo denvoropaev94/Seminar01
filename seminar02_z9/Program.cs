6cf54a5d249408411adaeaf2bc4cd1f41e562b13
«Denis» <denvoropaev7@gmail.com>
2022-08-10 11:35:02 +0300

    Добавили метод для решения задачи на нахождение максимальной цифры числа

---
+++ seminar02_z9/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая выводит случайное число из отрезка [10,99]
+и показывает наибольшее число.
+12>2, 78>8
+*/
+
+ int GetMaxNumberRandomValue()
+ {
+ int randomValue = new Random().Next(10,100);
+ Console.WriteLine($"RandomValue - {randomValue}");
+
+ int lastNumber = randomValue % 10;
+ int firstNumber = randomValue/10;
+ int max = firstNumber;
+
+ if (lastNumber > max){
+    max = lastNumber;
+ } 
+ if (lastNumber == firstNumber){
+     Console.WriteLine("Цифры равны!!!");
+ }
+ Console.WriteLine($"FirstNumber - {firstNumber}");
+ Console.WriteLine($"LastNumber - {lastNumber}");
+ Console.WriteLine($"Maximalnoe - {max}");
+ return max;
+ }
+
+int number = GetMaxNumberRandomValue();
+Console.WriteLine(number);
