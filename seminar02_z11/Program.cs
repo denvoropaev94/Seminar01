9b84e28faa221ae2968a8c32f7eb41a2f3bb3212
«Denis» <denvoropaev7@gmail.com>
2022-08-10 12:27:26 +0300

    Задача про удаление второй циры слачайного 3-х значного числа

---
+++ seminar02_z11/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу , которая выводит случайное трехзначное число и удаляет вторую цифру.
+// 782>72
+
+void GetThreedigitNumberRandomDeleteTwo(){
+    Random random = new Random();
+    int randomValue = random.Next(100,1000);
+    Console.WriteLine(randomValue);
+
+    int firstNumber = randomValue/100;
+    int lastNumber = randomValue%10;
+    Console.WriteLine($"{firstNumber}{lastNumber}");
+}
+GetThreedigitNumberRandomDeleteTwo();
+GetThreedigitNumberRandomDeleteTwo();
