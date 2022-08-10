1ab3c58554df95c08212d1d7dd212261698f648e
«Denis» <denvoropaev7@gmail.com>
2022-08-10 14:58:38 +0300

    Задача на проверку кратности числа на 7 и 21

---
+++ seminar02_z16/Program.cs
@@ -0,0 +1,16 @@
+﻿void CheckIsItSquare(int num1, int num2)
+{
+    if ((num1 == num2 * num2) || (num2 == num1 * num1))
+    {
+        Console.WriteLine("Является квадратом");
+    }
+    else
+    {
+        Console.WriteLine("НЕТ!");
+    }
+}
+Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+CheckIsItSquare(number1,number2);
