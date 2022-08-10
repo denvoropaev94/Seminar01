ce54e0c5b90a09b24218cfd4db96020380caa33d
«Denis» <denvoropaev7@gmail.com>
2022-08-10 15:29:13 +0300

    Задача написать программу, которая принимает 3-х значное число и на выходе показывает 3-ю цифру или сообщает что ее нет

---
+++ seminar02_dz13/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+void ShowsThirdDigit()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string stringNumber = Convert.ToString(number);
+    if (stringNumber.Length > 2)
+    {
+
+        Console.WriteLine("третья цифра этого числа - " + stringNumber[2]);
+    }
+    else
+    {
+
+    Console.WriteLine("Третьей цифры НЕТ! ");
+    }
+}
+ShowsThirdDigit();
