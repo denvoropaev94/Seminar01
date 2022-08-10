3b35c4ceb4ce93cf8f604fe15b146189eac1963b
«Denis» <denvoropaev7@gmail.com>
2022-08-10 15:20:10 +0300

    Задача написать программу, которая принимает 3-х значное число и на выходе показывает вторую цифру

---
+++ seminar02_dz10/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+void ShowsSecondDigit()
+{
+    Console.Write("Введи трёхзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string stringNumber = Convert.ToString(number);
+    Console.WriteLine("вторая цифра этого числа - " + stringNumber[1]);
+}
+ShowsSecondDigit();
