979f647453a3d01f51dc58c16f20108922045bc2
«Denis» <denvoropaev7@gmail.com>
2022-08-10 14:44:44 +0300

    Задача на проверку кратности числа на 7 и 21

---
+++ seminar02_z14/Program.cs
@@ -0,0 +1,15 @@
+﻿void ChecksMultipleNumber(int num){
+
+    if ((num%7==0)&(num%23==0))
+    {
+        Console.WriteLine("Число кратно 7 и 23 Ура!!!");
+    } 
+    else
+    {
+        Console.WriteLine("Число не кратно 7 и 23");
+    }
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+ChecksMultipleNumber(number);
